064207feb2f364684a871e946fde2a12464845f5
chgrtrd11 <131791000+chgrtrd11@users.noreply.github.com>
2023-09-05 21:51:39 +0800

    Add files via upload

---
+++ E-commerce Book Shop/includes/aside.php
@@ -0,0 +1,14 @@
+<?php
+    include_once("connection.php");
+    include_once "session.php";
+?>
+
+<aside>
+    <div class="pad-layout">
+        <div class="scrollDiv">
+            <div class="scrollContent">
+            
+            </div>
+        </div>
+    </div>
+</aside>
